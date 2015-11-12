12b51a877f08d8eb96505e8f57481da620fcaf3c
Leong Chin Poh <rayson@didc.fsktm.um.edu.my>
2015-11-12 15:19:07 +0800

    Added google search scripts

---
+++ google.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+function google { 
+     Q="$@"; 
+     GOOG_URL='https://www.google.com/search?q=';
+     stream=$(exo-open "${GOOG_URL}${Q//\ /+}" | grep -oP '\/url\?q=.+?&amp' | sed 's|/url?q=||; s|&amp||'); 
+     echo -e "${stream//\%/\x}"; 
+}
+google rayson
